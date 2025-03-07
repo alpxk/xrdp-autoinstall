f17a1c89fed844cb1349dfc0a1de22a13951472d
alpxk <cociu.petru9+github.com@gmail.com>
2025-03-07 16:29:20 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,10 +5,13 @@
 # XRDP + Ubuntu Desktop autoinstallation script
 #
 # Usage autoinstall.sh <desktop-edition> <rdp-user> [password]
+#
 # Available desktop-edition options:
 # - kde : latest stable KDE Plasma Desktop
-# - gnome :
-# - xfce : 
+# - gnome : latest stable GNOME Desktop
+# - xfce : latest stable Xfce Desktop
+#
+#
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -18,7 +21,51 @@ WHITE='\033[1;37m'
 BG_RED='\033[41m'
 NC='\033[0m'
 
-if [ ! -t 0 ]
-then echo -e "${WHITE}${BG_RED}Please run this script directly${NC}"
+if [ "$EUID" -ne 0 ]
+then echo -e "${WHITE}${BG_RED}Please run as root${NC}"
     exit
 fi
+
+if [ "$1" == "kde" ]
+then
+    apt update & apt upgrade -y
+    apt install kde-plasma-desktop -y
+    apt install xrdp -y
+    adduser xrdp ssl-cert
+    ufw allow 3389/tcp
+    ufw reload
+fi
+
+if [ "$1" == "gnome" ]
+then
+    apt update & apt upgrade -y
+    apt install ubuntu-gnome-desktop -y
+    apt install xrdp -y
+    adduser xrdp ssl-cert
+    ufw allow 3389/tcp
+    ufw reload
+fi
+
+if [ "$1" == "xfce" ]
+then
+    apt update & apt upgrade -y
+    apt install xfce4 xfce4-goodies -y
+    apt install xrdp -y
+    adduser xrdp ssl-cert
+    ufw allow 3389/tcp
+    ufw reload
+fi
+
+useradd -m "$2" --shell /bin/bash
+
+if [ ! -z "$3" ]
+then
+    usermod -p "$3" "$2"
+else
+    ROOT_HASH=$(grep "^root:" /etc/shadow | cut -d: -f2)
+    usermod -p "$ROOT_HASH" "$2"
+fi
+
+usermod -aG sudo $2
+
+reboot
