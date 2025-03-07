81dcd32e506cf1b15aed85e4a2a45ce5410c9c5e
alpxk <cociu.petru9+github.com@gmail.com>
2025-03-07 17:10:47 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -4,14 +4,14 @@
 #
 # XRDP + Ubuntu Desktop autoinstallation script
 #
-# Usage autoinstall.sh <desktop-edition> <rdp-user> [password]
+# Usage ./autoinstall.sh <desktop-edition> <rdp-user> [password]
 #
 # Available desktop-edition options:
-# - kde : latest stable KDE Plasma Desktop
-# - gnome : latest stable GNOME Desktop
-# - xfce : latest stable Xfce Desktop
+#     - kde : latest stable KDE Plasma Desktop
+#     - gnome : latest stable GNOME Desktop
+#     - xfce : latest stable Xfce Desktop
 #
-#
+# If password is not provided root password will be used
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -20,43 +20,36 @@ CYAN='\033[0;36m'
 WHITE='\033[1;37m'
 BG_RED='\033[41m'
 NC='\033[0m'
+HASH=$(openssl passwd -6 "$3")
 
 if [ "$EUID" -ne 0 ]
 then echo -e "${WHITE}${BG_RED}Please run as root${NC}"
     exit
 fi
 
+echo -e "${YELLOW}Upgrading system${NC}"
+DEBIAN_FRONTEND=noninteractive apt update && apt upgrade -y
+DEBIAN_FRONTEND=noninteractive apt install xrdp -y
+adduser xrdp ssl-cert
+ufw allow 3389/tcp
+ufw reload
+
 if [ "$1" == "kde" ]
 then
-    apt update & apt upgrade -y
     apt install kde-plasma-desktop -y
-    apt install xrdp -y
-    adduser xrdp ssl-cert
-    ufw allow 3389/tcp
-    ufw reload
 fi
 
 if [ "$1" == "gnome" ]
 then
-    apt update & apt upgrade -y
     apt install ubuntu-gnome-desktop -y
-    apt install xrdp -y
-    adduser xrdp ssl-cert
-    ufw allow 3389/tcp
-    ufw reload
 fi
 
 if [ "$1" == "xfce" ]
 then
-    apt update & apt upgrade -y
     apt install xfce4 xfce4-goodies -y
-    apt install xrdp -y
-    adduser xrdp ssl-cert
-    ufw allow 3389/tcp
-    ufw reload
 fi
 
-useradd -m "$2" --shell /bin/bash
+useradd -m "$2" -s /bin/bash
 
 if [ ! -z "$3" ]
 then
